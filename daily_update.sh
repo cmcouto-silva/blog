c34a884bf3f00980b1a4bb1dbd393735f047218b
cmcouto-silva <cmcouto.silva@gmail.com>
2020-03-18 18:54:12 +0100

    set automatic script for daily update

---
+++ daily_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update site
+R -e 'blogdown::build_site()'
+
+# add modification to stage area
+git add .
+
+# commit modifications
+git commit -m'daily update'
+
+# pushing modification to github
+git push origin master
+
