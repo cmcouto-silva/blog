11765419f99d1867ac61206e1c804eeb26a3aede
cmcouto-silva <cmcouto.silva@gmail.com>
2020-04-03 14:05:35 -0300

    daily update

--- schedule.sh
+++ schedule.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-for h in {06..23}; do
-	at $h:00 -f ./daily_update.sh
-done
-
-at 00:00 -f ./daily_update.sh
-at 00:01 -f ./schedule.sh
+at 10:00 -f ./daily_update.sh
+at 14:00 -f ./daily_update.sh
+at 18:00 -f ./daily_update.sh
+at 19:00 -f ./schedule.sh
